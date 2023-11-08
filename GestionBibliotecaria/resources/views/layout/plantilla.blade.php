b9c2e4108d4306fde9bad9c276bc7f285c6341ee
Kate1622 <katherineteran06@gmail.com>
2023-11-08 16:07:38 -0500

    zzzz

--- GestionBibliotecaria/resources/views/layout/plantilla.blade.php
+++ GestionBibliotecaria/resources/views/layout/plantilla.blade.php
@@ -234,6 +234,14 @@
                 </a>
               </li>
             </ul>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{route('graficoA')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Reportes</p>
+                </a>
+              </li>
+            </ul>
           </li>
           @if(auth()->user()->roles->Descripcionrol == 'ROLE_ADMIN')
           <li class="nav-item">
