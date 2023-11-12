ba87538ad39fa41e15478bd00e2ce1babcced718
Kate1622 <katherineteran06@gmail.com>
2023-11-11 21:27:27 -0500

    AAAA

--- GestionBibliotecaria/app/Http/Controllers/GraficoAbastecimientoController.php
+++ GestionBibliotecaria/app/Http/Controllers/GraficoAbastecimientoController.php
@@ -3,22 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Libro;
+use App\Models\Proveedor;
+use App\Models\registroDetallePedido;
 use App\Models\registroPedido;
 use Illuminate\Http\Request;
 
 class GraficoAbastecimientoController extends Controller
 {
-    public function index(){
-        $librosPrestadosPorFecha = registroPedido::select(\DB::raw('MONTHNAME(Fecha) as mes'),
-          \DB::raw('COUNT(PedidoID) as total'))
-         ->groupBy(\DB::raw('MONTHNAME(Fecha)'))
-         ->get();
+    public function index()
+    {
+        $librosPrestadosPorFecha = registroPedido::select(
+            \DB::raw('MONTHNAME(Fecha) as mes'),
+            \DB::raw('COUNT(PedidoID) as total')
+        )
+            ->groupBy(\DB::raw('MONTHNAME(Fecha)'))
+            ->get();
+
         $data = [];
+
         foreach ($librosPrestadosPorFecha as $libro) {
             $data[] = [
                 'mes' => $libro->mes,
-                'total' => $libro->total];}
-        return view ('Abastecimiento.Reportes',compact('data'));
+                'total' => $libro->total
+            ];
+        }
+
+        $libros = Libro::all();
+
+        $pedidos = registroPedido::all();
+
+        $pedidosConDatos = [];
+
+        foreach ($pedidos as $pedido) {
+
+            $cantidadTotal = 0;
+
+            $detalles = registroDetallePedido::where('PedidoID', $pedido->PedidoID)->get();
+
+            foreach ($detalles as $detalle) {
+                $cantidadTotal += $detalle->Cantidad;
+            }
+
+            $pedidosYProveedores[] = [
+                'Proveedor' => Proveedor::find($pedido->ProveedorID)->Empresa,
+                'Fecha' => $pedido->Fecha,
+                'Cantidad' => $cantidadTotal,
+            ];
+        }
+
+        return view('Abastecimiento.Reportes', compact('data', 'libros', 'pedidosYProveedores'));
     }
 }
 
--- GestionBibliotecaria/resources/views/Abastecimiento/Reportes.blade.php
+++ GestionBibliotecaria/resources/views/Abastecimiento/Reportes.blade.php
@@ -1,64 +1,143 @@
 @extends('layout.plantilla')
 
-@section('titulo','ListadoPedido')
+@section('titulo', 'ListadoPedido')
 @section('contenido')
-
-<DIV class="ROW"> 
-    <DIV class="COL-6">
-      <P></P>
-    </DIV>
-    <div class="COL-6"  style="width: 480PX; margin-left:180px;">
-      <p style="margin-left: 100px;">CANTIDAD DE PEDIDOS POR MES</p>
-        <canvas id="myChart" width="50" height="30"></canvas>
-    </div>
-</div>     
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-      
+  <CENTER>
+  <p>REPORTES</p>
+  </CENTER>
+ 
+    <div class="container mt-5 d-flex flex-wrap">
+
+        <!-- Informe 1 -->
+        <div class="col" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">CANTIDAD DE PEDIDOS POR MES</h5>
+                <canvas id="PedidosPorMes" width="50" height="30"></canvas>
+            </div>
+        </div>
+
+        <!-- Informe 2 -->
+        <div class="col" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Stock de libros</h5>
+                <canvas id="StockDeLibros" width="50" height="30"></canvas>
+            </div>
+        </div>
+
+        <!-- Informe 3 -->
+        <div class="col" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Cantidad de pedidos por proveedor</h5>
+                <canvas id="ProveedorPedidos" width="50" height="30"></canvas>
+            </div>
+        </div>
+    </div>
+
     <script>
         const data = @json($data);
-        const ctx = document.getElementById('myChart');
+        const ctx = document.getElementById('PedidosPorMes');
         new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: data.map(item => item.mes),
-            datasets: [{
-              label: '# de libros',
-              data: data.map(item => item.total),
-              backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(201, 203, 207, 0.2)'
-              ],
-              borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)'
-              ],
-              borderWidth: 1
-  }]
-             
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
+            type: 'bar',
+            data: {
+                labels: data.map(item => item.mes),
+                datasets: [{
+                    label: '# de libros',
+                    data: data.map(item => item.total),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(255, 159, 64)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)',
+                        'rgb(201, 203, 207)'
+                    ],
+                    borderWidth: 1
+                }]
+
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
             }
-          }
         });
-    </script>
 
-<BR></BR>
-<a  style=" margin-left:425px;" class="btn btn-primary" href="{{route('prueba')}}">GenerarPDF</a>
+        const libros = @json($libros);
+        const ctxLibros = document.getElementById('StockDeLibros');
+        new Chart(ctxLibros, {
+            type: 'pie',
+            data: {
+                labels: libros.map(libro => libro.Titulo),
+                datasets: [{
+                    label: 'Stock de libros',
+                    data: libros.map(item => item.Stock),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
 
+                    borderWidth: 1
+                }]
 
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
 
+        const pedidos = @json($pedidosYProveedores);
+        const ctxPedidos = document.getElementById('ProveedorPedidos');
+        new Chart(ctxPedidos, {
+            type: 'bar',
+            data: {
+                labels: pedidos.map(pedido => pedido.Proveedor),
+                datasets: [{
+                    label: '# De Pedidos',
+                    data: pedidos.map(pedido => pedido.Cantidad),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                    ],
+
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+
+    <a style=" margin-left:425px;" class="btn btn-primary" href="{{ route('prueba') }}">GenerarPDF</a>
 @endsection
